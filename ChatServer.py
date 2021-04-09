1ac39a9c9e237e2c955f81bada9f55be6486af87
ParadoxEngineer <45982206+ParadoxEngineer@users.noreply.github.com>
2021-04-09 16:01:21 -0500

    4/9-Edwards- Bug fixing
    Program is runnable! Server now accepts a single users Join request, accepts, and closes.

--- ChatServer.py
+++ ChatServer.py
@@ -3,8 +3,8 @@ import socket
 import threading
 import struct
 
-def clientHandler():
-	
+#def clientHandler():
+#	
 
 def main():
 	MAXLINE = 4096
@@ -20,19 +20,32 @@ def main():
 	
 	found = False
 	while True:
-		data = struct.unpack('!Bhs', conn.recv(MAXLINE)
+		data = struct.unpack('!Bh', conn.recv(3))
+		print(data)
+		nameLen = data[1]
+		namePackType = '!' + str(nameLen) + 's'
+		attemptName = struct.unpack(namePackType, conn.recv(MAXLINE))
+		attemptName = attemptName[0].decode('ASCII')
 		
-		if data[0] = 0:
-			for e in usernames:
-				if e[0] = data[2]:
-					#Join fail
-					conn.send(struct.pack('!B', 1))
-					found = True
-					break
+		if data[0] == 0:
+			if len(usernames) == 0:
+				#Join success
+				print(str(attemptName) + ' joined the server')
+				conn.send(struct.pack('!B', 0))
+				usernames.append((attemptName, addr))
+				break
+			else:
+				for e in usernames:
+					if e[0] == attemptName:
+						#Join fail
+						conn.send(struct.pack('!B', 1))
+						found = True
+						break
 			if not found:
 				#Join success
+				print(str(attemptName) + ' joined the server')
 				conn.send(struct.pack('!B', 0))
-				usernames.add((data[2], addr))
+				usernames.append((attemptName, addr))
 				break
 	
 	#Send user joined message
@@ -40,4 +53,4 @@ def main():
 	conn.close()
 	
 if __name__ == "__main__":
-	main() 
+	main()
