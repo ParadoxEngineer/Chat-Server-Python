8ae76d6c3cccd7f0a34ade94cff9e8fdd2f8f716
ParadoxEngineer <45982206+ParadoxEngineer@users.noreply.github.com>
2021-04-16 11:10:18 -0500

    4/16-Jonathan Edwards- Cleaned the merge
    Cleaned out the merge.

--- ChatServer.py
+++ ChatServer.py
@@ -4,10 +4,8 @@ import threading
 import struct
 import select
 
+#Maybe uncessecary
 usernames = []
-
-#Leftover for threading
-#def clientHandler():
 
 #Sends given message to all users (for Join/Leave notices and Talk)
 def sendMessage(message):
@@ -19,7 +17,6 @@ def sendMessage(message):
 		usernames[e][2].send(struct.pack(packString, 2, messageLen, message))
 
 def main():
-<<<<<<< HEAD
     MAXLINE = 4096
     PORT = 9000
     usernames = []
@@ -48,7 +45,8 @@ def main():
             #If it is client socket, recv whatever message 
             else:    
                 data = struct.unpack('!Bh', sock.recv(3))
-                print(data) #FOR DEBUGGING ONLY, REMOVE LATER   
+		#Debug STUB
+                #print(data)  
                 messageLen = data[1]
                 messageType = '!' + str(messageLen) + 's'
                 message = struct.unpack(messageType, sock.recv(struct.calcsize(messageType)))
@@ -104,68 +102,9 @@ def main():
                     sock.send(reply[0])
         if reply:
             reply.pop(0)
-                       
-       
-                    
+                                      
 	#close server socket	
-    sock.close()
-=======
-	MAXLINE = 4096
-	PORT = 9000
-	
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind(('', PORT))
-	sock.listen(32)
-	
-	conn, addr = sock.accept()
-	
-	####
-	#This should all be in thread func
-	####
-	
-	#Receive Join command
-	found = False
-	while True:
-		data = struct.unpack('!Bh', conn.recv(3))
-		
-		#Debug STUB
-		#print(data)
-		nameLen = data[1]
-		namePackType = '!' + str(nameLen) + 's'
-		attemptName = struct.unpack(namePackType, conn.recv(MAXLINE))
-		attemptName = attemptName[0].decode('ASCII')
-		
-		if data[0] == 0:
-			if len(usernames) == 0:
-				#Join success
-				print(str(attemptName) + ' joined the server')
-				conn.send(struct.pack('!B', 0))
-				usernames.append((attemptName, addr, conn))
-				break
-			else:
-				for e in usernames:
-					if e[0] == attemptName:
-						#Join fail
-						conn.send(struct.pack('!B', 1))
-						found = True
-						break
-			if not found:
-				#Join success
-				print(str(attemptName) + ' joined the server')
-				conn.send(struct.pack('!B', 0))
-				usernames.append((attemptName, addr))
-				break
-	
-	#Send user joined message
-	
-	#Main loop
-	
-	####
-	#End of thread section
-	####
-	
-	conn.close()
->>>>>>> 6bc021a2f3139a0acd1a46b91fb46cf2c12bce77
+	sock.close()
 	
 if __name__ == "__main__":
 	main()
