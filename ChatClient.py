6bb7051d83e28a1299cbc1bee7578df88d1ee238
ParadoxEngineer <45982206+ParadoxEngineer@users.noreply.github.com>
2021-04-16 11:15:50 -0500

    4/16-Jonathan Edwards- Cleaned merge
    Cleaned the merge.

--- ChatClient.py
+++ ChatClient.py
@@ -25,8 +25,6 @@ def outputHandler(sock):
         
 
 def main():
-<<<<<<< HEAD
-
     if len(sys.argv) < 2:
         IP = input('Enter server IP: ')
     else:
@@ -40,17 +38,19 @@ def main():
 
     
     while True:
-        #Send username to the server, 0 is the join command
-        packType = '!Bh' + str(len(username)) + 's'
-        sock.send(struct.pack(packType, 0, len(username), username.encode('ASCII')))
-        
-        #Receive respond from server whether the name is taken or not
-        data = struct.unpack('!B', sock.recv(struct.calcsize('!B')))
-        if data[0] == 0:
-            
-            break
-        
-        username = input('Username rejected, enter a new username: ')
+        if len(username) > 0:
+		#Send username to the server, 0 is the join command
+		packType = '!Bh' + str(len(username)) + 's'
+		sock.send(struct.pack(packType, 0, len(username), username.encode('ASCII')))
+
+		#Receive respond from server whether the name is taken or not
+		data = struct.unpack('!B', sock.recv(struct.calcsize('!B')))
+		if data[0] == 0:
+		    break
+
+		username = input('Username rejected, enter a new username: ')
+	else:
+		username = input('Username cannot be blank, enter a username: ')
 
     #Get user inputs
     inputThread = threading.Thread(target=inputHandler, args=(sock, username))
@@ -67,39 +67,6 @@ def main():
 
     root.mainloop()
     sock.close()
-=======
-	MAXLINE = 4096
-	
-	if len(sys.argv) < 2:
-		IP = input('Enter server IP: ')
-	else:
-		IP = sys.argv[1]
-	PORT = 9000
-	
-	username = input('Enter your username: ')
-	
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((IP, PORT))
-	
-	# Join command
-	while True:
-		if len(username) > 0:
-			packType = '!Bh' + str(len(username)) + 's'
-			sock.send(struct.pack(packType, 0, len(username), username.encode('ASCII')))
-			
-			#Debug STUB
-			#print(struct.pack(packType, 0, len(username), username.encode('ASCII')))
-			
-			data = struct.unpack('!B', sock.recv(MAXLINE))
-			if data[0] == 0:
-				break
-			
-			username = input('Username rejected, enter a new username: ')
-		else:
-			username = input('Username cannot be blank, enter a username: ')
-	
-	sock.close()
->>>>>>> 6bc021a2f3139a0acd1a46b91fb46cf2c12bce77
 
 if __name__ == "__main__":
     main()
