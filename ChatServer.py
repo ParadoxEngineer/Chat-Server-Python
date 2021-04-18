114f65080a833aca545d551daa0ba882b3c54287
Cor Sawai <corsawai@letu.edu>
2021-04-17 20:11:41 -0500

    list and direct

--- ChatServer.py
+++ ChatServer.py
@@ -6,9 +6,6 @@ import select
 
 usernames = []
 
-#Leftover for threading
-#def clientHandler():
-
 #Sends given message to all users (for Join/Leave notices and Talk)
 def sendMessage(message):
 	messageLen = len(message)
@@ -19,12 +16,12 @@ def sendMessage(message):
 		usernames[e][2].send(struct.pack(packString, 2, messageLen, message))
 
 def main():
-<<<<<<< HEAD
     MAXLINE = 4096
     PORT = 9000
     usernames = []
     sockets = []
     reply = [] #Hold all the messages in bytes (already packed)
+    flags = [] #Hold which type of message to send back to client. Helpful for direct and list.
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('', PORT))
@@ -96,76 +93,34 @@ def main():
                                 message = '[' + name[0] + ']' + message
                                 packType = '!Bh' + str(len(message)) + 's'
                                 reply.append(struct.pack(packType, 2, len(message), message.encode('ASCII'))) 
+
+                if data[0] == 3:
+                    message = ''
+                    for n in username:
+                        message = message + ',' + n[0]
+                    for name in usernames:
+                        if name[1].fileno() == sock.fileno():
+                            packType = '!Bh' + str(len(message)) + 's'
+                            listOfNames.append((sock, struct.pack(packType, 3, len(message), message.encode('ASCII'))))
+
         
         #If there is a message in the list, send all of them one at a time
         for sock in writes:
             if sock.fileno() != sfd: 
                 if reply:             
                     sock.send(reply[0])
+                if listOfNames:
+                    for s in listOfNames:
+                        if sock == s[0]:
+                            sock.send(s[1])
+                            s.pop(0)
+
         if reply:
             reply.pop(0)
                        
-       
-                    
+            
 	#close server socket	
     sock.close()
-=======
-	MAXLINE = 4096
-	PORT = 9000
-	
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind(('', PORT))
-	sock.listen(32)
-	
-	conn, addr = sock.accept()
-	
-	####
-	#This should all be in thread func
-	####
-	
-	#Receive Join command
-	found = False
-	while True:
-		data = struct.unpack('!Bh', conn.recv(3))
-		
-		#Debug STUB
-		#print(data)
-		nameLen = data[1]
-		namePackType = '!' + str(nameLen) + 's'
-		attemptName = struct.unpack(namePackType, conn.recv(MAXLINE))
-		attemptName = attemptName[0].decode('ASCII')
-		
-		if data[0] == 0:
-			if len(usernames) == 0:
-				#Join success
-				print(str(attemptName) + ' joined the server')
-				conn.send(struct.pack('!B', 0))
-				usernames.append((attemptName, addr, conn))
-				break
-			else:
-				for e in usernames:
-					if e[0] == attemptName:
-						#Join fail
-						conn.send(struct.pack('!B', 1))
-						found = True
-						break
-			if not found:
-				#Join success
-				print(str(attemptName) + ' joined the server')
-				conn.send(struct.pack('!B', 0))
-				usernames.append((attemptName, addr))
-				break
-	
-	#Send user joined message
-	
-	#Main loop
-	
-	####
-	#End of thread section
-	####
-	
-	conn.close()
->>>>>>> 6bc021a2f3139a0acd1a46b91fb46cf2c12bce77
 	
 if __name__ == "__main__":
 	main()
--- ui.py
+++ ui.py
@@ -69,9 +69,17 @@ class App:
     def sendMessage(self):
         #Get message from user and send it to server
         messageToSend = ''
+        prtclNo = 2
         messageToSend = self.userInput.get('1.0', 'end')
+        #Set the type of message
+        temp = messageToSend.split()
+        if temp[0] == '@list':
+            prtclNo = 3
+        elif temp[0][0] == '@':
+            prtclNo = 4
+
         packType = '!Bh' + str(len(messageToSend)) + 's'
-        self.sock.send(struct.pack(packType, 2, len(messageToSend), messageToSend.encode('ASCII')))
+        self.sock.send(struct.pack(packType, prtclNo, len(messageToSend), messageToSend.encode('ASCII')))
 
     def updateChatScreen(self):
         while True:
